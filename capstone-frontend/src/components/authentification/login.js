bb0a0629d48d40bb8cab13a8cc18d598400336f0
ak0757 <asmaakhaloua@my.unt.edu>
2022-02-22 14:33:45 -0600

    Sprint 1 | merge Branches

--- capstone-frontend/src/components/authentification/login.js
+++ capstone-frontend/src/components/authentification/login.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import {Button,Alert,  Col, Row,Container} from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { onAuthStateChanged,signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase-config";
+import loginbg from "../../img/login-bg.PNG";
+import { createBrowserHistory } from 'history';
+const history = createBrowserHistory();
 export default class login extends Component {
   constructor(props) {
     super(props);
-    this.state = { loginEmail: "", loginPassword: "" };
+    this.state = { loginEmail: "", loginPassword: "" , user:null,signError:"",showError:false};
+    
   }
-
+   
   handleInput = (e) => {
     const name = e.target.name;
 
@@ -16,21 +22,57 @@ export default class login extends Component {
     this.setState({ [name]: value });
     // console.log("Name: " + name + "value:" + value);
   };
-  signin() {}
+  signin = (event) => {
+    
+      signInWithEmailAndPassword(  
+         auth,
+        this.state.loginEmail,
+        this.state.loginPassword
+      ).then(user =>{
+        console.log("login"+this.state.user?.email);
+        history.push("/dashboard/"+this.state.user)
+      })
+      .catch(error => {
+        let message = error.message +"..."+error.code;
+        console.log("error"+error.message);
+        if(error.code === "auth/invalid-email" || error.code === "auth/user-not-found"){
+     
+           message = "Sorry, we couldn't find user with that credentials.Please try again"
+        }
+  
+        this.setState({signError:message,showError:true});
+    
+      });
+    
+    
+
+  }
+  componentDidMount() {
+    onAuthStateChanged(auth,(currentUser)=>{
+      this.setState({user:currentUser});
+    });
+  }
 
   render() {
+    
     return (
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Form>
+      <Container className="auth_container">
+  <Row>
+    <Col>  <img src={loginbg} className="signupbg" alt="sign up Background"></img></Col>
+    <Col className="auth-inner-col"> <div className="auth-inner">
+        <Form>
           <h3>Sign In</h3>
+          <Alert show={this.state.showError} variant="danger" >
+          {this.state.signError}
+  </Alert>
+        
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control
                 type="email"
                 placeholder="Enter email"
                 name="loginEmail"
-                onChange={this.handleInput}
+                onChange={this.handleInput} required
               />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
@@ -43,22 +85,27 @@ export default class login extends Component {
                 type="password"
                 placeholder="Password"
                 name="loginPassword"
-                onChange={this.handleInput}
+                onChange={this.handleInput} required
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Form.Group className="mb-3">
+            <Button variant="primary" type="button" onClick={this.signin}>
               Submit
             </Button>
+            </Form.Group>
           </Form>
           <div className="w-100  mt-2">
             Don't have an account?
-            <Link to="/signup"> Register </Link>
+            <Link to="signup"> Register </Link>
           </div>
         </div>
-      </div>
+        </Col>
+  </Row>
+ 
+</Container>
     );
   }
 }
